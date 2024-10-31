708f3860864461adeac825ce163b7b9e130022db
Lordaka <karmelsensei@gmail.com>
2024-10-31 12:24:42 +0100

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -26,21 +26,21 @@ module.exports = {
 	langs: {
 		vi: {
 			added: "✅ | Đã thêm quyền admin cho %1 người dùng:\n%2",
-			alreadyAdmin: "\n⚠️ | %1 người dùng đã có quyền admin từ trước rồi:\n%2",
-			missingIdAdd: "⚠️ | Vui lòng nhập ID hoặc tag người dùng muốn thêm quyền admin",
+			alreadyAdmin: "\n⚠ | %1 người dùng đã có quyền admin từ trước rồi:\n%2",
+			missingIdAdd: "⚠ | Vui lòng nhập ID hoặc tag người dùng muốn thêm quyền admin",
 			removed: "✅ | Đã xóa quyền admin của %1 người dùng:\n%2",
-			notAdmin: "⚠️ | %1 người dùng không có quyền admin:\n%2",
-			missingIdRemove: "⚠️ | Vui lòng nhập ID hoặc tag người dùng muốn xóa quyền admin",
+			notAdmin: "⚠ | %1 người dùng không có quyền admin:\n%2",
+			missingIdRemove: "⚠ | Vui lòng nhập ID hoặc tag người dùng muốn xóa quyền admin",
 			listAdmin: "👑 | Danh sách admin:\n%1"
 		},
 		en: {
-			added: "✅ | 𝐚𝐣𝐨𝐮𝐭𝐞 𝐚𝐮 𝐫𝐨𝐥𝐞 𝐥'𝐚𝐝𝐦𝐢𝐧 𝐞𝐬𝐭 %1 users:\n%2",
-			alreadyAdmin: "\n⚠️ | %1 𝐚𝐥𝐞𝐫𝐭𝐞𝐬 𝐢𝐥 𝐞𝐬𝐭 𝐝𝐞́𝐣𝐚̀ 𝐚𝐝𝐦𝐢𝐧 :\n%2",
-			missingIdAdd: "⚠️ | 𝐬𝐭𝐩 𝐭𝐚𝐠 𝐥𝐚 𝐩𝐞𝐫𝐬𝐨𝐧𝐧𝐞 𝐪𝐮𝐞 𝐯𝐨𝐮𝐬 𝐬𝐨𝐮𝐡𝐚𝐢𝐭𝐞 𝐦𝐞̀𝐭𝐫𝐞 𝐚𝐮𝐱 𝐚𝐝𝐦𝐢𝐧𝐬  𝐨𝐮 𝐮𝐭𝐢𝐥𝐢𝐬𝐞 𝐬𝐨𝐧 𝐮𝐢𝐝 !",
-			removed: "✅ | 𝐞𝐧𝐥𝐞𝐯𝐞 𝐚𝐮 𝐫𝐨𝐥𝐞 𝐝𝐞𝐬 𝐚𝐝𝐦𝐢𝐧𝐬 𝐞𝐬𝐭  %1 users:\n%2",
-			notAdmin: "⚠️ | %1 users don't have admin role:\n%2",
-			missingIdRemove: "⚠️ | 𝐬𝐭𝐩 𝐭𝐚𝐠 𝐥𝐚 𝐩𝐞𝐫𝐬𝐨𝐧𝐧𝐞 𝐪𝐮𝐞 𝐯𝐨𝐮𝐬 𝐬𝐨𝐮𝐡𝐚𝐢𝐭𝐞 𝐞𝐧𝐥𝐞𝐯𝐞 𝐚𝐮𝐱 𝐫𝐨𝐥𝐞𝐬 𝐝𝐞𝐬 𝐚𝐝𝐦𝐢𝐧𝐬 𝐨𝐮 𝐮𝐭𝐢𝐥𝐢𝐬𝐞 𝐬𝐨𝐧 𝐮𝐢𝐝 !",
-			listAdmin: "🔖| 𝐀𝐃𝐌𝐈𝐍𝐒 𝐃𝐔 𝐁𝐎𝐓:\n%1"
+			added: "🧸 | 𝐋𝐞𝐬  𝐚𝐝𝐦𝐢𝐧𝐬 𝐝𝐮 𝐜𝐥𝐚𝐧\n%2",
+			alreadyAdmin: "\n⚠ | %1 𝐚𝐥𝐞𝐫𝐭𝐞 𝐭'𝐞𝐬 𝐝𝐞́𝐣𝐚̀ 𝐚𝐝𝐦𝐢𝐧 𝐝𝐮 𝐛𝐨𝐭 :\n%2",
+			missingIdAdd: "⚠ | 𝐏𝐥𝐞𝐚𝐬𝐞 𝐞𝐧𝐭𝐞𝐫 𝐈𝐃 𝐨𝐫 𝐭𝐚𝐠 𝐮𝐬𝐞𝐫 𝐭𝐨 𝐚𝐝𝐝 𝐚𝐝𝐦𝐢𝐧 𝐫𝐨𝐥𝐞",
+			removed: "🏷️ | 𝐋'𝐚𝐝𝐦𝐢𝐧 𝐚𝐲𝐚𝐧𝐭𝑛𝑡 𝐞𝐭𝐞 𝐫𝐞𝐭𝐢𝐫𝐞 𝐞𝐬𝐭\n%2",
+			notAdmin: "⚠ | %1 𝐮𝐬𝐞𝐫𝐬 𝐝𝐨𝐧'𝐭 𝐡𝐚𝐯𝐞 𝐚𝐝𝐦𝐢𝐧 𝐫𝐨𝐥𝐞:\n%2",
+			missingIdRemove: "⚠ | 𝐏𝐥𝐞𝐚𝐬𝐞 𝐞𝐧𝐭𝐞𝐫 𝐈𝐃 𝐨𝐫 𝐭𝐚𝐠 𝐮𝐬𝐞𝐫 𝐭𝐨 𝐫𝐞𝐦𝐨𝐯𝐞 𝐚𝐝𝐦𝐢𝐧 𝐫𝐨𝐥𝐞",
+			listAdmin: "🔮| 𝐀𝐃𝐌𝐈𝐍𝐒 𝐁𝐎𝐓:\n━━━━━━━━━━━━━━━\n%1"
 		}
 	},
 
@@ -107,10 +107,10 @@ module.exports = {
 			case "list":
 			case "-l": {
 				const getNames = await Promise.all(config.adminBot.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
-				return message.reply(getLang("listAdmin", getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")));
+				return message.reply(getLang("listAdmin", getNames.map(({ uid, name }) => `웃 ${name} 『${uid}』`).join("\n")));
 			}
 			default:
 				return message.SyntaxError();
 		}
 	}
-};
+		}
